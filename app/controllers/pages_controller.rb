b547e6d0a8e50b43bc64db9cec40a42b5d063fb9
Harry Ryder <harrymryder@gmail.com>
2019-01-04 00:35:44 +0000

    devise routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
   end
 
   def about
+  end
+
+  def articles
+  end
+
+  def books
+  end
+
+  def film
   end
 end
