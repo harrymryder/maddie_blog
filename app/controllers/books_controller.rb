263ac96020a4a116dc3025c4f4a9e44e06ea58ac
Harry Ryder <harrymryder@gmail.com>
2019-01-14 10:55:26 +0000

    Book edit and delete actions and links

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   skip_before_action :authenticate_user!
+  before_action :set_book, only: [:edit, :update, :destroy]
 
   def index
     @books = Book.order(created_at: :desc)
@@ -23,11 +24,27 @@ class BooksController < ApplicationController
   end
 
   def update
+    @book.update(book_params)
+
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to books_path
   end
 
   private
 
   def book_params
     params.require(:book).permit(:title, :author, :description, :photo)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
   end
 end
